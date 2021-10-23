3275160fddeb66a85f4b2a60b3a685373f327123
thoriqzs25 <thoriqzahfran@yahoo.com>
2021-10-24 03:16:07 +0700

    using authSlice

--- socialmedia/src/components/Layout/MainHeader.js
+++ socialmedia/src/components/Layout/MainHeader.js
@@ -2,15 +2,28 @@ import Dot from '../UI/Dot';
 import Search from '../UI/Search';
 import classes from './MainHeader.module.css';
 import user from '../../assets/user.png';
+import home from '../../assets/home.png'
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/auth-slice';
 
 const MainHeader = (props) => {
+    const dispatch = useDispatch();
+
+    const isAuth = useSelector(state => state.auth.isAuthenticated);
+    const toggleHandler = (event) => {
+        event.preventDefault();
+
+        dispatch(authActions.toggle());
+    };
+    console.log(isAuth)
     return (
         <header className={classes.header}>
             <h1>SOCIAL MEDIA</h1>
             <nav>
                 <ul>
                     <li><Search /></li>
-                    <li><Dot item={user} alt={"user_alt"}/></li>
+                    {!isAuth && <li onClick={toggleHandler}><Dot item={user} alt={"user_alt"} /></li>}
+                    {isAuth && <li onClick={toggleHandler}><Dot item={home} alt={"home_alt"} /></li>}
                 </ul>
             </nav>
         </header>
