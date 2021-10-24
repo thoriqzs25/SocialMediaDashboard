3f2ff627ae7915bfca7753773a38d0d2aa1896c2
thoriqzs25 <thoriqzahfran@yahoo.com>
2021-10-24 13:07:46 +0700

    adding scroll in album

--- socialmedia/src/components/Layout/Overlays/AlbumOverlay.js
+++ socialmedia/src/components/Layout/Overlays/AlbumOverlay.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Card from "../../UI/Card";
 import Modal from "../../UI/Modal/Modal";
 import classes from './AlbumOverlay.module.css';
 
 const AlbumOverlay = (props) => {
     const [albumList, setAlbumList] = useState([]);
-
+     
     useEffect(() => {
         const fetchAlbum = async () => {
             const getAlbum = await fetch('https://jsonplaceholder.typicode.com/albums');
             const albumJSON = await getAlbum.json();
-    
+            
             const albumData = [];
-
+            
             for (const key in albumJSON) {
                 albumData.push({
                     id: key,
@@ -24,20 +24,40 @@ const AlbumOverlay = (props) => {
         };
         fetchAlbum();
     }, []);
+
+
+    const myRef = useRef();
+        
+    const executeScroll = () => {
+        myRef.current.scrollIntoView({ behavior: 'smooth' })
+    };
+
     
     return (
         <Modal>
             <div className={classes.album}>
                 {albumList.map((album, index) => {
                     return(
-                    <>
-                    {album.userId === 1 && 
-                    <Card>
-                        <p>index</p>
-                        <p>{album.title}</p>
-                    </Card>}
-                    </>)
+                    <div key={album.id}>
+                        {album.userId === 1 && index === 0 &&
+                            <div ref={myRef}>
+                                <Card>
+                                    <p>{index}</p>
+                                    <p>{album.title}</p>
+                                </Card>
+                            </div>
+                        }
+                        {album.userId === 1 && index >= 1 &&
+                            <div>
+                                <Card>
+                                    <p>{index}</p>
+                                    <p>{album.title}</p>
+                                </Card>
+                            </div>
+                        }
+                    </div>)
                 })}
+                <button onClick={executeScroll}> Click to scroll </button>
             </div>
         </Modal>
     );
